bd26deaf094dcb7e9a86b02768613de4afc4846a
Sachin Pravin Patel <sppatel@apache.org>
2006-01-09 19:53:59 +0000

    fix sec plan version
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/devtools/trunk/modules/eclipse-plugin@367375 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/org.apache.geronimo.devtools.eclipse.core/src/org/apache/geronimo/core/internal/GeronimoSchemaNS.java
+++ plugins/org.apache.geronimo.devtools.eclipse.core/src/org/apache/geronimo/core/internal/GeronimoSchemaNS.java
@@ -30,7 +30,7 @@ public class GeronimoSchemaNS {
     
     public static final String GERONIMO_NAMING_NS = "http://geronimo.apache.org/xml/ns/naming-1.0";
     
-    public static final String GERONIMO_SECURITY_NS = "http://geronimo.apache.org/xml/ns/security-1.0";
+    public static final String GERONIMO_SECURITY_NS = "http://geronimo.apache.org/xml/ns/security-1.1";
     
     public static final String GERONIMO_DEPLOYMENT_NS = "http://geronimo.apache.org/xml/ns/deployment-1.0";
     
