d255e1ccb20fa81c652d9f64ed9ed43b71bd2512
Sachin Pravin Patel <sppatel@apache.org>
2006-04-21 14:09:10 +0000

    refactor for 1.1
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/devtools/eclipse-plugin/trunk@395891 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/org.apache.geronimo.st.core/src/org/apache/geronimo/st/core/GenericGeronimoServerRuntime.java
+++ plugins/org.apache.geronimo.st.core/src/org/apache/geronimo/st/core/GenericGeronimoServerRuntime.java
@@ -15,17 +15,55 @@
  */
 package org.apache.geronimo.st.core;
 
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jst.server.generic.core.internal.GenericServerRuntime;
 
-public class GenericGeronimoServerRuntime extends GenericServerRuntime implements
-		IGeronimoRuntime {
-	
-	/* (non-Javadoc)
+public class GenericGeronimoServerRuntime extends GenericServerRuntime
+		implements IGeronimoRuntime {
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.wst.server.core.model.RuntimeDelegate#validate()
 	 */
 	public IStatus validate() {
+		String version = detectVersion();
+		if (!getRuntime().getRuntimeType().getVersion().equals(version))
+			return new Status(IStatus.ERROR, Activator.PLUGIN_ID, 0, null, null);
 		return super.validate();
 	}
 
+	public String detectVersion() {
+		File libDir = getRuntime().getLocation().append("lib").toFile();
+		if (libDir.exists()) {
+			File[] libs = libDir.listFiles();
+			URL systemjarURL = null;
+			for (int i = 0; i < libs.length; i++) {
+				if (libs[i].getName().startsWith("geronimo-system")) {
+					try {
+						systemjarURL = libs[i].toURL();
+						break;
+					} catch (MalformedURLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			URLClassLoader cl = new URLClassLoader(new URL[] { systemjarURL });
+			try {
+				Class clazz = cl.loadClass("org.apache.geronimo.system.serverinfo.ServerConstants");
+				Method method = clazz.getMethod("getVersion", new Class[] {});
+				return (String) method.invoke(null, null);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
 }
