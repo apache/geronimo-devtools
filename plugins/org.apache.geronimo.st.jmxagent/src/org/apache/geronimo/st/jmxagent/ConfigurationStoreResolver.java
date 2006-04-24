0c24f29ef277002a798ff03e2f2fdafbe319180c
Sachin Pravin Patel <sppatel@apache.org>
2006-04-24 20:01:42 +0000

    add jmx agent plugin
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/devtools/eclipse-plugin/trunk@396671 13f79535-47bb-0310-9956-ffa450edef68

---
+++ plugins/org.apache.geronimo.st.jmxagent/src/org/apache/geronimo/st/jmxagent/ConfigurationStoreResolver.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright 2004, 2005 The Apache Software Foundation or its licensors, as applicable
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.geronimo.st.jmxagent;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.geronimo.kernel.repository.Artifact;
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jst.j2ee.componentcore.util.EARArtifactEdit;
+import org.eclipse.jst.j2ee.internal.project.J2EEProjectUtilities;
+import org.eclipse.wst.common.componentcore.internal.StructureEdit;
+import org.eclipse.wst.common.componentcore.internal.WorkbenchComponent;
+import org.eclipse.wst.common.componentcore.internal.impl.WorkbenchComponentImpl;
+import org.eclipse.wst.common.componentcore.resources.IVirtualReference;
+
+public class ConfigurationStoreResolver implements ConfigurationStoreResolverMBean {
+
+	public Set resolve(Artifact artifact, String module, String path) {
+		IProject project = getProject(artifact);
+
+		if (project == null) {
+			// error couldn't find project for artifact throw exception
+		}
+
+		if (module != null && J2EEProjectUtilities.isEARProject(project)) {
+			EARArtifactEdit edit = EARArtifactEdit.getEARArtifactEditForRead(project);
+			try {
+				IVirtualReference[] refs = edit.getComponentReferences();
+				for (int i = 0; i < refs.length; i++) {
+					IVirtualReference ref = refs[i];
+					String moduleURI = edit.getModuleURI(ref.getReferencedComponent());
+					if (moduleURI.equals(module)) {
+						// found module
+						project = ref.getReferencedComponent().getProject();
+					}
+				}
+			} finally {
+				if (edit != null)
+					edit.dispose();
+			}
+		}
+
+		Set urls = new HashSet();
+
+		if (path == null || path.length() == 0) {
+			StructureEdit moduleCore = StructureEdit.getStructureEditForRead(project);
+			try {
+				WorkbenchComponent component = moduleCore.getComponent();
+				IPath loc = ((WorkbenchComponentImpl) component).getDefaultSourceRoot();
+				try {
+					URL url = project.findMember(loc).getLocation().toFile().toURL();
+					urls.add(url);
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				}
+
+			} finally {
+				if (moduleCore != null)
+					moduleCore.dispose();
+			}
+		} else {
+			// return output containers
+			IContainer[] containers = J2EEProjectUtilities.getOutputContainers(project);
+			for (int i = 0; i < containers.length; i++) {
+				try {
+					urls.add(containers[i].getLocation().toFile().toURL());
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return urls;
+
+	}
+
+	public IProject getProject(Artifact artifact) {
+		// get IProject from IModule with persisted configID
+		return null;
+	}
+}
---
+++ plugins/org.apache.geronimo.st.jmxagent/src/org/apache/geronimo/st/jmxagent/JMXAgent.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright 2004, 2005 The Apache Software Foundation or its licensors, as applicable
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.geronimo.st.jmxagent;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import javax.management.MBeanServer;
+import javax.management.MBeanServerFactory;
+import javax.management.ObjectName;
+import javax.management.remote.JMXConnectorServer;
+import javax.management.remote.JMXConnectorServerFactory;
+import javax.management.remote.JMXServiceURL;
+
+
+public class JMXAgent {
+
+	private static JMXAgent INSTANCE = null;
+
+	private static MBeanServer server = null;
+	private static JMXServiceURL url = null;
+
+	private JMXConnectorServer connectorServer = null;
+
+	private static int DEFAULT_PORT = 8090;
+
+	private JMXAgent() {
+		init();
+	}
+
+	public static JMXAgent getInstance() {
+		if (INSTANCE == null)
+			INSTANCE = new JMXAgent();
+		return INSTANCE;
+	}
+
+	private void init() {
+		try {
+			url = new JMXServiceURL("hessian", null, DEFAULT_PORT, "/hessian");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		server = MBeanServerFactory.createMBeanServer();
+		loadBean(ConfigurationStoreResolver.class.getName(), "ConfigStoreResolver:name=resolver");
+	}
+
+	public void start() throws IOException {
+		if (connectorServer == null) 
+			connectorServer = JMXConnectorServerFactory.newJMXConnectorServer(url, null, server);
+		
+		if (!connectorServer.isActive())
+			connectorServer.start();
+	}
+
+	public void stop() throws IOException {
+		if (connectorServer != null)
+			connectorServer.stop();
+	}
+
+	public void loadBean(String className, String objectName) {
+		try {
+			server.createMBean(className, new ObjectName(objectName));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
