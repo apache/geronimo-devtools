3f4012a88f85ab3907124abcccea76485d9eeb57
Sachin Pravin Patel <sppatel@apache.org>
2007-01-04 17:44:53 +0000

    validate manifest's bundleclasspath after required libs are copied into bundle
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/devtools/eclipse-plugin/branches/1.2.1@492650 13f79535-47bb-0310-9956-ffa450edef68

--- maven-plugins/maven-geronimodevtools-plugin/src/main/java/org/apache/geronimo/eclipse/devtools/BundleManifestMojo.java
+++ maven-plugins/maven-geronimodevtools-plugin/src/main/java/org/apache/geronimo/eclipse/devtools/BundleManifestMojo.java
@@ -18,6 +18,9 @@ package org.apache.geronimo.eclipse.devtools;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
@@ -28,7 +31,7 @@ import org.apache.maven.project.MavenProject;
 import org.osgi.framework.Constants;
 
 /**
- *@goal validatemanifest
+ * @goal validatemanifest
  */
 public class BundleManifestMojo extends AbstractMojo {
 
@@ -43,20 +46,17 @@ public class BundleManifestMojo extends AbstractMojo {
 	 * @required
 	 */
 	private File manifestFile;
-	
+
 	/**
 	 * @parameter
 	 */
-	private File classpathEntriesDir;
-	
-	/**
-	 * @parameter
-	 */
-	private boolean includeRootClasspathEntry = false;
-	
+	private String classpathEntriesDir;
+
 	private Attributes attributes = null;
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.apache.maven.plugin.Mojo#execute()
 	 */
 	public void execute() throws MojoExecutionException, MojoFailureException {
@@ -64,29 +64,43 @@ public class BundleManifestMojo extends AbstractMojo {
 			FileInputStream fis = new FileInputStream(manifestFile);
 			Manifest manifest = new Manifest(fis);
 			attributes = manifest.getMainAttributes();
-			
 			validate(Constants.BUNDLE_SYMBOLICNAME, project.getName() + ";singleton:=true");
-			//validate(Constants.BUNDLE_NAME, project.getName());
 			validate(Constants.BUNDLE_VERSION, project.getVersion());
-			
-			if(classpathEntriesDir != null && classpathEntriesDir.exists()) {
-				File entries[] = classpathEntriesDir.listFiles();
-				if(entries.length > 0) {
-					if(includeRootClasspathEntry) {
-						
-					}
-				}
-			}
-			
+			validateBundleClasspath();
 		} catch (Exception e) {
 			throw new MojoExecutionException(e.getMessage(), e);
 		}
 	}
-	
+
+	private void validateBundleClasspath() throws MojoFailureException {
+		if (classpathEntriesDir == null)
+			return;
+		File libs[] = new File(project.getBasedir(), classpathEntriesDir).listFiles();
+		if (libs == null)
+			return;
+		String classpath = attributes.getValue(Constants.BUNDLE_CLASSPATH);
+		StringTokenizer tokenizer = new StringTokenizer(classpath, ",");
+		List cpEntries = new ArrayList();
+		while (tokenizer.hasMoreTokens()) {
+			cpEntries.add(tokenizer.nextToken());
+		}
+		List missingEntries = new ArrayList();
+		for (int i = 0; i < libs.length; i++) {
+			File lib = libs[i];
+			String expectedEntry = classpathEntriesDir + "/" + lib.getName();
+			if (!cpEntries.contains(expectedEntry)) {
+				missingEntries.add(expectedEntry);
+			}
+		}
+		if (!missingEntries.isEmpty()) {
+			throw new MojoFailureException("Manifest Bundle-Classpath is missing the following entries: " + missingEntries.toString());
+		}
+	}
+
 	private void validate(String attribute, String correctValue) throws MojoFailureException {
 		String currentValue = attributes.getValue(attribute).replaceAll(" ", "");
 		correctValue = correctValue.replaceAll(" ", "");
-		if(!correctValue.equals(currentValue)) {
+		if (!correctValue.equals(currentValue)) {
 			throw new MojoFailureException("Attribute value for " + attribute + " in bundle manifest is incorrect. [Found: " + currentValue + "] [Expected: " + correctValue + "]");
 		}
 	}
