8f8567c8540fe535faa6360f0bdb7bbad517c704
Tim McConnell <mcconne@apache.org>
2008-04-11 01:39:01 +0000

    GERONIMODEVTOOLS-312 First junit testcase for schema conversion
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/devtools/eclipse-plugin/trunk@647028 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/org.apache.geronimo.deployment.v21.jaxbmodel/src/test/java/org/apache/geronimo/jee/application/GeronimoApplicationTest.java
+++ plugins/org.apache.geronimo.deployment.v21.jaxbmodel/src/test/java/org/apache/geronimo/jee/application/GeronimoApplicationTest.java
@@ -26,13 +26,22 @@ import java.io.InputStream;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.ValidationEvent;
+import javax.xml.bind.ValidationEventHandler;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.sax.SAXSource;
 
 import junit.framework.AssertionFailedError;
 import junit.framework.TestCase;
 
 import org.apache.geronimo.jee.deployment.Module;
 import org.custommonkey.xmlunit.Diff;
+import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.XMLFilterImpl;
+import org.xml.sax.XMLReader;
 
 /**
  * <strong>GeronimoApplicationTest</strong> is used to test various JAXB 
@@ -45,6 +54,7 @@ import org.xml.sax.InputSource;
  * <p>The following JAXB operations are performed: 
  * <ol>
  *      <li>Unmarshalling and marshalling sequence
+ *      <li>Namespace conversion
  * </ol>
  * 
  * 
@@ -52,17 +62,31 @@ import org.xml.sax.InputSource;
  */
 public class GeronimoApplicationTest extends TestCase {
 
-    public void testUnmarshallMarshall() throws Exception {
+    public void testUnmarshallAndMarshall() throws Exception {
 
-    	unmarshallAndMarshall("application/geronimo-application-example-1.xml", 
+        unmarshallAndMarshall("application/geronimo-application-example-1.xml", 
                               "application/geronimo-application-expected-1.xml");
-    	
-    	unmarshallAndMarshall("application/geronimo-application-example-2.xml", 
-                           	  "application/geronimo-application-expected-2.xml");
-    	
-    	unmarshallAndMarshall("application/geronimo-application-example-3.xml", 
-                           	  "application/geronimo-application-expected-3.xml");
+
+        unmarshallAndMarshall("application/geronimo-application-example-2.xml", 
+                              "application/geronimo-application-expected-2.xml");
+
+        unmarshallAndMarshall("application/geronimo-application-example-3.xml", 
+                              "application/geronimo-application-expected-3.xml");
     }
+
+
+    public void testConvertNamespace() throws Exception {
+
+        convertNamespace("application/geronimo-application-example-4.xml",
+                         "application/geronimo-application-expected-1.xml");
+
+        convertNamespace("application/geronimo-application-example-5.xml", 
+                         "application/geronimo-application-expected-2.xml");
+
+        convertNamespace("application/geronimo-application-example-6.xml", 
+                         "application/geronimo-application-expected-3.xml");
+    }
+
 
     private void unmarshallAndMarshall(String fileExample, String fileExpected) throws Exception {
 
@@ -112,6 +136,67 @@ public class GeronimoApplicationTest extends TestCase {
             System.out.println("[Actual XML] " + '\n' + actual + '\n');
             throw e;            
         }
+
+    }
+
+    private  void convertNamespace(String fileExample,String fileExpected) throws Exception {
+
+        // 
+        // Create unmarshaller and marshaller
+        // 
+        JAXBContext jAXBContext = JAXBContext.newInstance(Application.class,Module.class);
+        Unmarshaller unmarshaller = jAXBContext.createUnmarshaller();
+        Marshaller marshaller = jAXBContext.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+
+        // 
+        // Create SAXParser
+        // 
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        factory.setNamespaceAware(true);
+        factory.setValidating(false);
+        SAXParser parser = factory.newSAXParser();
+
+        // 
+        // Create NamespaceFilter to filter for v1.1 namespaces
+        // 
+        NamespaceFilter xmlFilter = new NamespaceFilter(parser.getXMLReader());
+
+        // 
+        // Read example and expected XML files
+        // 
+        InputStream exampleInputStream = this.getClass().getClassLoader().getResourceAsStream(fileExample);
+        InputStream expectedInputStream = this.getClass().getClassLoader().getResourceAsStream(fileExpected);
+        String example = readContent(exampleInputStream);
+        String expected = readContent(expectedInputStream);
+
+        // 
+        // Unmarshall the example file
+        // 
+        SAXSource source = new SAXSource(xmlFilter, new InputSource(new ByteArrayInputStream(expected.getBytes())));
+        Object jAXBElement = unmarshaller.unmarshal(source);
+
+        // 
+        // Marshall the output of the unmarshall
+        // 
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        marshaller.marshal(jAXBElement, baos);
+        byte[] bytes = baos.toByteArray();
+        String actual = new String(bytes);
+
+        // 
+        // Compare actual and expected
+        // 
+        try {
+            Diff myDiff = new Diff(expected, actual);
+            assertTrue("Files are similar " + myDiff, myDiff.similar());
+        } catch (AssertionFailedError e) {
+            System.out.println("[Example XML] " + '\n' + example + '\n');
+            System.out.println("[Expected XML] " + '\n' + expected + '\n');
+            System.out.println("[Actual XML] " + '\n' + actual + '\n');
+            throw e;            
+        }
     }
 
     private String readContent(InputStream in) throws IOException {
@@ -125,4 +210,23 @@ public class GeronimoApplicationTest extends TestCase {
         String content = sb.toString();
         return content;
     }
+
+    public static class NamespaceFilter extends XMLFilterImpl {
+        private static final InputSource EMPTY_INPUT_SOURCE = new InputSource(new ByteArrayInputStream(new byte[0]));
+
+        public NamespaceFilter(XMLReader xmlReader) {
+            super(xmlReader);
+        }
+
+        public void startElement(String uri, String localName, String qname, Attributes atts) throws SAXException {
+            if (uri.equals("http://geronimo.apache.org/xml/ns/j2ee/application-1.2")) {
+                uri = "http://geronimo.apache.org/xml/ns/j2ee/application-2.0";
+            }
+            else if (uri.equals("http://geronimo.apache.org/xml/ns/deployment-1.1")) {
+                uri = "http://geronimo.apache.org/xml/ns/deployment-1.2";
+            }
+            super.startElement(uri, localName, qname, atts);
+        }
+    }
+
 }
