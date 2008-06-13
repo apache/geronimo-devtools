4f77dc6a0df81ccf0c6ac1b967be06db2d836a7b
Donald Woods <dwoods@apache.org>
2008-06-13 12:27:56 +0000

    GERONIMODEVTOOLS-348 JSF Tags Not Displayed in Eclipse Web Page Editor.  BJ, thanks for the patch.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/devtools/eclipse-plugin/trunk@667502 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/org.apache.geronimo.st.core/src/main/java/org/apache/geronimo/st/core/GeronimoServerRuntimeTargetHandler.java
+++ plugins/org.apache.geronimo.st.core/src/main/java/org/apache/geronimo/st/core/GeronimoServerRuntimeTargetHandler.java
@@ -109,6 +109,7 @@ public class GeronimoServerRuntimeTargetHandler extends RuntimeClasspathProvider
                 IPath jaxwsApiSpec = runtime.getLocation().append("repository/org/apache/axis2/axis2-jaxws-api/");
                 IPath jstlSpec     = runtime.getLocation().append("repository/jstl/jstl/");
                 IPath myfacesSpec  = runtime.getLocation().append("repository/org/apache/myfaces/core/myfaces-api/");
+                IPath myfacesImplSpec  = runtime.getLocation().append("repository/org/apache/myfaces/core/myfaces-impl/");
                 IPath jdbcSpec     = runtime.getLocation().append("repository/org/apache/geronimo/framework/geronimo-jdbc/");
                 addLibraryEntries(list, javaMailSpec.toFile(), true);
                 addLibraryEntries(list, jaxbApiSpec.toFile(),  true);
@@ -116,6 +117,7 @@ public class GeronimoServerRuntimeTargetHandler extends RuntimeClasspathProvider
                 addLibraryEntries(list, jaxwsApiSpec.toFile(), true);
                 addLibraryEntries(list, jstlSpec.toFile(),     true);
                 addLibraryEntries(list, myfacesSpec.toFile(),  true);
+                addLibraryEntries(list, myfacesImplSpec.toFile(),  true);
                 addLibraryEntries(list, jdbcSpec.toFile(),     true);
             }
         }
