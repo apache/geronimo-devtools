84c96b72c7e32189d5d3e7f8f3c3bc8b88c0dfd2
Barrett Jay Reed <bjreed@apache.org>
2008-11-18 19:45:11 +0000

    GERONIMODEVTOOLS-400 change for updating deployment plan editors when Source page has been saved
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/devtools/eclipse-plugin/trunk@718691 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/org.apache.geronimo.st.ui/src/main/java/org/apache/geronimo/st/ui/editors/AbstractGeronimoDeploymentPlanEditor.java
+++ plugins/org.apache.geronimo.st.ui/src/main/java/org/apache/geronimo/st/ui/editors/AbstractGeronimoDeploymentPlanEditor.java
@@ -27,6 +27,8 @@ import org.apache.geronimo.st.core.operations.ImportDeploymentPlanDataModelProvi
 import org.apache.geronimo.st.core.operations.ImportDeploymentPlanOperation;
 import org.apache.geronimo.st.ui.internal.Messages;
 import org.apache.geronimo.st.ui.internal.Trace;
+import org.apache.geronimo.st.ui.pages.AbstractGeronimoFormPage;
+import org.apache.geronimo.st.ui.pages.DeploymentPlanSourcePage;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -37,7 +39,7 @@ import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormEditor;
 import org.eclipse.ui.forms.editor.IFormPage;
@@ -55,163 +57,203 @@ import org.eclipse.wst.sse.ui.StructuredTextEditor;
  */
 public abstract class AbstractGeronimoDeploymentPlanEditor extends FormEditor {
 
-	protected JAXBElement deploymentPlan;
+    protected JAXBElement deploymentPlan;
 
-	public AbstractGeronimoDeploymentPlanEditor() {
-		super();
-	}
+    public AbstractGeronimoDeploymentPlanEditor() {
+        super();
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.part.EditorPart#doSave(org.eclipse.core.runtime.IProgressMonitor)
-	 */
-	public void doSave(IProgressMonitor monitor) {
-		InputStream is = null;
-		try {
-			IEditorInput input = getEditorInput();
-			if (input instanceof IFileEditorInput) {
-				IFileEditorInput fei = (IFileEditorInput) input;
-				if (deploymentPlan != null) {
-					saveDeploymentPlan(fei.getFile());
-					commitFormPages(true);
-				}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.part.EditorPart#doSave(org.eclipse.core.runtime.IProgressMonitor)
+     */
+    public void doSave(IProgressMonitor monitor) {
+        InputStream is = null;
+        try {
+            IEditorInput input = getEditorInput();
+            if (input instanceof IFileEditorInput) {
+                IFileEditorInput fei = (IFileEditorInput) input;
+                if (deploymentPlan != null) {
+                    saveDeploymentPlan(fei.getFile());
+                    commitFormPages(true);
+                }
 
-				if (getActiveEditor() == null) {
-					editorDirtyStateChanged();
-				} else {
-					getActiveEditor().doSave(monitor);
-					if (deploymentPlan != null) {
-//						if (deploymentPlan.eResource() != null) {
-//							deploymentPlan.eResource().unload();
-//						}
-						// TODO not sure if this is the best way to refresh
-						// model
-						deploymentPlan = loadDeploymentPlan(fei.getFile());
-					}
-				}
-			}
-		} catch (Exception e) {
-			Trace.trace(Trace.SEVERE, "Error saving", e);
-		} finally {
-			try {
-				if (is != null)
-					is.close();
-			} catch (Exception e) {
-				// do nothing
-			}
-		}
-	}
+                if (getActiveEditor() == null) {
+                    editorDirtyStateChanged();
+                } else {
+                    getActiveEditor().doSave(monitor);
+                    if (deploymentPlan != null) {
+//                        if (deploymentPlan.eResource() != null) {
+//                            deploymentPlan.eResource().unload();
+//                        }
+                        // TODO not sure if this is the best way to refresh
+                        // model
+                        deploymentPlan = loadDeploymentPlan(fei.getFile());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Trace.trace(Trace.SEVERE, "Error saving", e);
+        } finally {
+            try {
+                if (is != null)
+                    is.close();
+            } catch (Exception e) {
+                // do nothing
+            }
+        }
+    }
 
-//	private void saveEditors(IFile file) throws IOException, JAXBException {
-//		JAXBContext jb = JAXBContext.newInstance( "org.apache.geronimo.xml.ns.j2ee.web_2_0:org.apache.geronimo.xml.ns.j2ee.application_2:org.apache.geronimo.xml.ns.deployment_1:org.apache.geronimo.xml.ns.naming_1", Activator.class.getClassLoader() );
-//		jb.createMarshaller().marshal( deploymentPlan, new File( file.getLocationURI().toURL().getFile()) );
-//		commitFormPages(true);
-//	}
+//    private void saveEditors(IFile file) throws IOException, JAXBException {
+//        JAXBContext jb = JAXBContext.newInstance( "org.apache.geronimo.xml.ns.j2ee.web_2_0:org.apache.geronimo.xml.ns.j2ee.application_2:org.apache.geronimo.xml.ns.deployment_1:org.apache.geronimo.xml.ns.naming_1", Activator.class.getClassLoader() );
+//        jb.createMarshaller().marshal( deploymentPlan, new File( file.getLocationURI().toURL().getFile()) );
+//        commitFormPages(true);
+//    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.forms.editor.FormEditor#addPages()
-	 */
-	protected void addPages() {
-		try {
-			doAddPages();
-		} catch (PartInitException e1) {
-			e1.printStackTrace();
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.forms.editor.FormEditor#addPages()
+     */
+    protected void addPages() {
+        try {
+            doAddPages();
+        } catch (PartInitException e1) {
+            e1.printStackTrace();
+        }
+    }
 
-	abstract public void doAddPages() throws PartInitException;
+    abstract public void doAddPages() throws PartInitException;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.part.EditorPart#doSaveAs()
-	 */
-	public final void doSaveAs() {
-		// do nothing
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.part.EditorPart#doSaveAs()
+     */
+    public final void doSaveAs() {
+        // do nothing
+    }
 
-	protected void addSourcePage() throws PartInitException {
-		StructuredTextEditor source = new StructuredTextEditor();
-		int index = addPage(source, getEditorInput());
-		setPageText(index, Messages.editorTabSource);
-	}
+    protected void addSourcePage() throws PartInitException {
+        DeploymentPlanSourcePage source = new DeploymentPlanSourcePage(this);
+        int index = addPage(source, getEditorInput());
+        setPageText(index, Messages.editorTabSource);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.part.EditorPart#isSaveAsAllowed()
-	 */
-	public boolean isSaveAsAllowed() {
-		return false;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.part.EditorPart#isSaveAsAllowed()
+     */
+    public boolean isSaveAsAllowed() {
+        return false;
+    }
 
-	public void commitFormPages(boolean onSave) {
-		IFormPage[] pages = getPages();
-		for (int i = 0; i < pages.length; i++) {
-			IFormPage page = pages[i];
-			IManagedForm mform = page.getManagedForm();
-			if (mform != null && mform.isDirty())
-				mform.commit(true);
-		}
-	}
+    public void commitFormPages(boolean onSave) {
+        IFormPage[] pages = getPages();
+        for (int i = 0; i < pages.length; i++) {
+            IFormPage page = pages[i];
+            IManagedForm mform = page.getManagedForm();
+            if (mform != null && mform.isDirty())
+                mform.commit(true);
+        }
+    }
 
-	public IFormPage[] getPages() {
-		ArrayList formPages = new ArrayList();
-		for (int i = 0; i < pages.size(); i++) {
-			Object page = pages.get(i);
-			if (page instanceof IFormPage)
-				formPages.add(page);
-		}
-		return (IFormPage[]) formPages.toArray(new IFormPage[formPages.size()]);
-	}
+    public IFormPage[] getPages() {
+        ArrayList formPages = new ArrayList();
+        for (int i = 0; i < pages.size(); i++) {
+            Object page = pages.get(i);
+            if (page instanceof IFormPage)
+                formPages.add(page);
+        }
+        return (IFormPage[]) formPages.toArray(new IFormPage[formPages.size()]);
+    }
 
-	public JAXBElement getDeploymentPlan() {
-		return deploymentPlan;
-	}
+    public JAXBElement getDeploymentPlan() {
+        return deploymentPlan;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.IEditorPart#init(org.eclipse.ui.IEditorSite,
-	 *      org.eclipse.ui.IEditorInput)
-	 */
-	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
-		super.init(site, input);
-		if (input instanceof IFileEditorInput) {
-			IFileEditorInput fei = (IFileEditorInput) input;
-			deploymentPlan = loadDeploymentPlan(fei.getFile());
-			
-			boolean fix = false;
-			if(deploymentPlan == null) {
-				fix = MessageDialog.openQuestion(Display.getDefault().getActiveShell(), Messages.errorOpenDialog, Messages.editorCorrect);
-			}
-			
-			if(fix) {
-				IProject project = fei.getFile().getProject();
-				IDataModel model = DataModelFactory.createDataModel(new ImportDeploymentPlanDataModelProvider());
-				model.setProperty(IFacetDataModelProperties.FACET_PROJECT_NAME, project.getName());
-				try {
-					IFacetedProject facetedProject = ProjectFacetsManager.create(project);
-					model.setProperty(IFacetProjectCreationDataModelProperties.FACET_RUNTIME, facetedProject.getPrimaryRuntime());
-					IDataModelOperation op = new ImportDeploymentPlanOperation(model);
-					op.execute(new NullProgressMonitor(), null);
-				} catch (Exception e) {
-					//ingnore
-				}
-				
-				deploymentPlan = loadDeploymentPlan(fei.getFile());
-				
-				if (deploymentPlan == null) {	
-					MessageDialog.openInformation(Display.getDefault().getActiveShell(), Messages.errorOpenDialog, Messages.editorDefault);
-				}
-			}
-		}
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.IEditorPart#init(org.eclipse.ui.IEditorSite,
+     *      org.eclipse.ui.IEditorInput)
+     */
+    public void init(IEditorSite site, IEditorInput input) throws PartInitException {
+        super.init(site, input);
+        if (input instanceof IFileEditorInput) {
+            IFileEditorInput fei = (IFileEditorInput) input;
+            deploymentPlan = loadDeploymentPlan(fei.getFile());
+            
+            boolean fix = false;
+            if(deploymentPlan == null) {
+                fix = MessageDialog.openQuestion(Display.getDefault().getActiveShell(), Messages.errorOpenDialog, Messages.editorCorrect);
+            }
+            
+            if(fix) {
+                IProject project = fei.getFile().getProject();
+                IDataModel model = DataModelFactory.createDataModel(new ImportDeploymentPlanDataModelProvider());
+                model.setProperty(IFacetDataModelProperties.FACET_PROJECT_NAME, project.getName());
+                try {
+                    IFacetedProject facetedProject = ProjectFacetsManager.create(project);
+                    model.setProperty(IFacetProjectCreationDataModelProperties.FACET_RUNTIME, facetedProject.getPrimaryRuntime());
+                    IDataModelOperation op = new ImportDeploymentPlanOperation(model);
+                    op.execute(new NullProgressMonitor(), null);
+                } catch (Exception e) {
+                    //ingnore
+                }
+                
+                deploymentPlan = loadDeploymentPlan(fei.getFile());
+                
+                if (deploymentPlan == null) {    
+                    MessageDialog.openInformation(Display.getDefault().getActiveShell(), Messages.errorOpenDialog, Messages.editorDefault);
+                }
+            }
+        }
+    }
 
-	abstract public JAXBElement loadDeploymentPlan(IFile file);
-	abstract public void saveDeploymentPlan(IFile file) throws IOException, JAXBException;
+    public void reloadDeploymentPlan() {
+        IEditorInput input = getEditorInput();
+        if (input instanceof IFileEditorInput) {
+            IFileEditorInput fei = (IFileEditorInput) input;
+            if (deploymentPlan != null) {
+                deploymentPlan = loadDeploymentPlan(fei.getFile());
+                IFormPage[] pages = getPages();
+                for (int i = 0; i < pages.length; i++) {
+                    IFormPage page = pages[i];
+                    IManagedForm mform = page.getManagedForm();
+                    if (mform != null) {
+                        if (page instanceof AbstractGeronimoFormPage) {
+                            AbstractGeronimoFormPage geronimoPage = (AbstractGeronimoFormPage)page;
+                            geronimoPage.refresh();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void pageChange(int newPageIndex) {
+        if (isDirty()) {
+            IFormPage[] pages = getPages();
+            IFormPage active = getActivePageInstance();
+            int curr = getCurrentPage();
+            if (getCurrentPage() == newPageIndex) {
+                return;
+            }
+            // if the old or the new page are the source page, don't allow the page change 
+            if (newPageIndex == pages.length || getCurrentPage() == pages.length) {
+                setActivePage(curr);
+                MessageDialog.openInformation(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), Messages.savePageTitle, Messages.savePageMessage);
+                return;
+            }
+        }
+        super.pageChange(newPageIndex);
+    }
+
+    abstract public JAXBElement loadDeploymentPlan(IFile file);
+    abstract public void saveDeploymentPlan(IFile file) throws IOException, JAXBException;
 
 }
