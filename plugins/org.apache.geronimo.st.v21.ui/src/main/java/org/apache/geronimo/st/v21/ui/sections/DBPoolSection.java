976f7a7dbea8ca4ecc1f1ff92d66de95d7b7696b
Xuan Dai <delos@apache.org>
2010-01-08 09:38:07 +0000

    add missing license headers for several files and update JIRA list in release notes
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/devtools/eclipse-plugin/branches/2.2@897145 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/org.apache.geronimo.st.v21.ui/src/main/java/org/apache/geronimo/st/v21/ui/sections/DBPoolSection.java
+++ plugins/org.apache.geronimo.st.v21.ui/src/main/java/org/apache/geronimo/st/v21/ui/sections/DBPoolSection.java
@@ -1,3 +1,21 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package org.apache.geronimo.st.v21.ui.sections;
 
 import java.util.ArrayList;
@@ -8,20 +26,14 @@ import javax.xml.bind.JAXBElement;
 
 import org.apache.geronimo.jee.application.ExtModule;
 import org.apache.geronimo.jee.connector.Connector;
-import org.apache.geronimo.jee.deployment.Gbean;
 import org.apache.geronimo.jee.deployment.Pattern;
-import org.apache.geronimo.st.ui.CommonMessages;
 import org.apache.geronimo.st.ui.sections.AbstractTableSection;
-import org.apache.geronimo.st.ui.sections.AbstractListSection.ContentProvider;
-import org.apache.geronimo.st.ui.sections.AbstractListSection.LabelProvider;
 import org.apache.geronimo.st.v21.ui.pages.ConnectorPage;
 import org.apache.geronimo.st.v21.ui.wizards.DBPoolWizard;
-import org.apache.geronimo.st.v21.ui.wizards.ExtModuleWizard;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.ui.forms.IFormPart;
 import org.eclipse.ui.forms.editor.FormEditor;
 import org.eclipse.ui.forms.widgets.FormToolkit;
