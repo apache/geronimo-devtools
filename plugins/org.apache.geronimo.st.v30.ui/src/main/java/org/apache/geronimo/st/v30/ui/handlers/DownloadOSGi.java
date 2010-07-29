19a81462f9424be28b7d7b8f370fe1b1bb7f8bf1
Xuan Dai <delos@apache.org>
2010-07-29 04:34:36 +0000

    GERONIMODEVTOOLS-619 correct blueprint editor name in extension.Thanks to Kan's patch
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/devtools/eclipse-plugin/trunk@980306 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/org.apache.geronimo.st.v30.ui/src/main/java/org/apache/geronimo/st/v30/ui/handlers/DownloadOSGi.java
+++ plugins/org.apache.geronimo.st.v30.ui/src/main/java/org/apache/geronimo/st/v30/ui/handlers/DownloadOSGi.java
@@ -17,6 +17,8 @@
 package org.apache.geronimo.st.v30.ui.handlers;
 
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 import org.apache.geronimo.st.v30.ui.internal.Messages;
@@ -24,10 +26,16 @@ import org.apache.geronimo.st.v30.ui.internal.Trace;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.equinox.p2.ui.ProvisioningUI;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.browser.IWebBrowser;
 import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
 import org.eclipse.ui.internal.browser.WorkbenchBrowserSupport;
+import org.eclipse.equinox.p2.operations.InstallOperation;
+import org.eclipse.equinox.p2.operations.RepositoryTracker;
+import org.eclipse.equinox.p2.metadata.IInstallableUnit;
+import org.eclipse.equinox.p2.core.ProvisionException;
+import org.eclipse.equinox.p2.repository.metadata.IMetadataRepository;
 
 /**
  * Handler to download the OSGi Application Development tools (i.e., Aries free tooling)
@@ -44,17 +52,34 @@ public class DownloadOSGi extends AbstractHandler  {
 
         String url = Messages.DownloadOSGiURL;
 
-        try {
             int style = IWorkbenchBrowserSupport.AS_EXTERNAL | IWorkbenchBrowserSupport.STATUS;
-            IWebBrowser browser = WorkbenchBrowserSupport.getInstance().createBrowser(style, "", "", "");
-            browser.openURL(new URL(url));
-        }
-        catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        catch (PartInitException e) {
-            e.printStackTrace();
-        }
+//            IWebBrowser browser = WorkbenchBrowserSupport.getInstance().createBrowser(style, "", "", "");
+//            browser.openURL(new URL(url));
+            ProvisioningUI provisioningUI = ProvisioningUI.getDefaultUI();
+            RepositoryTracker tracker = provisioningUI.getRepositoryTracker();
+            URI[] uri=new URI[1];
+            try {
+                uri[0]=new URI("http://public.dhe.ibm.com/ibmdl/export/pub/software/rational/OSGiAppTools");
+            } catch (URISyntaxException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            tracker.addRepository(uri[0], null, provisioningUI.getSession());
+           // InstallOperation installOperation = provisioningUI.getInstallOperation(null,uri);
+            provisioningUI.openInstallWizard(null, null, null);
+            try {
+                provisioningUI.loadMetadataRepository(uri[0],true,null);
+            } catch (ProvisionException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+           // tracker.refreshRepositories(arg0, provisioningUI., arg2);
+//        catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+//        catch (PartInitException e) {
+//            e.printStackTrace();
+//        }
 
         Trace.tracePoint("Exit", "DownloadOSGi.execute");
         return null;
