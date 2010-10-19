9030a06002044ca32168f180206eaaa77da64cc1
Xuan Dai <delos@apache.org>
2010-10-19 03:09:43 +0000

    GERONIMODEVTOOLS-637 add missing files for aries test cases
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/devtools/eclipse-plugin/trunk@1024098 13f79535-47bb-0310-9956-ffa450edef68

---
+++ testsuite/plugins/org.apache.geronimo.testsuite.v30/src/main/java/org/apache/geronimo/testsuite/v30/ui/aries/NewOSGiCompositeBundleTest.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.geronimo.testsuite.v30.ui.aries;
+
+import org.apache.geronimo.testsuite.common.ui.AbbotHelper;
+import org.apache.geronimo.testsuite.common.ui.Constants;
+import org.apache.geronimo.testsuite.common.ui.ServerTasks;
+import org.apache.geronimo.testsuite.common.ui.WorkbenchTasks;
+import org.eclipse.swt.widgets.Shell;
+import org.junit.Before;
+import org.junit.Test;
+
+import abbot.swt.eclipse.junit.extensions.WorkbenchTestCase;
+import abbot.swt.eclipse.utils.WorkbenchUtilities;
+
+/*
+ * New cba test.
+ * @version $Rev$ $Date$
+ */
+public class NewOSGiCompositeBundleTest extends WorkbenchTestCase {
+	Shell workbenchShell;
+	AbbotHelper abbotHelper;
+	ServerTasks serverTasks;
+	WorkbenchTasks workbenchTasks;
+
+	@Before()
+	public void setUp() {
+		workbenchShell = WorkbenchUtilities.getWorkbenchWindow().getShell();
+		abbotHelper = new AbbotHelper(workbenchShell);
+		serverTasks = new ServerTasks(workbenchShell, abbotHelper, Constants.SERVER_V30 );
+	}
+	
+	@Test()
+	public void testNewOSGiCompositeBundle() {
+		assertTrue(installServer());
+		//TODO create, deploy, run, undeploy cba
+		assertTrue(uninstallServer());
+	}
+	
+	private boolean installServer() {
+		boolean success = serverTasks.createServer();
+		if(success == true) {
+			success = serverTasks.startServer(false);
+		}
+		return success;
+	}
+	
+	private boolean uninstallServer() {
+		boolean success = serverTasks.stopServer();
+        if (success == true) {
+            success = serverTasks.removeServer();
+        }
+        return success;
+	}
+	
+}
