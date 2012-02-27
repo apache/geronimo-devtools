2bdc3e3d0912577bce73ac96c7eea2d7a8076d48
Yi Xiao <johnxiao@apache.org>
2012-02-27 10:02:17 +0000

    clean up the code.
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/devtools/eclipse-plugin/branches/3.0-beta-1@1294085 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/org.apache.geronimo.st.v30.core/src/main/java/org/apache/geronimo/st/v30/core/GeronimoServerBehaviourDelegate.java
+++ plugins/org.apache.geronimo.st.v30.core/src/main/java/org/apache/geronimo/st/v30/core/GeronimoServerBehaviourDelegate.java
@@ -1279,9 +1279,9 @@ public class GeronimoServerBehaviourDelegate extends ServerBehaviourDelegate imp
     public boolean isFullyStarted() {
         if (isKernelAlive()) {
             AbstractNameQuery query = new AbstractNameQuery(PersistentConfigurationList.class.getName());
-            Set<AbstractName> configLists = kernel.listGBeans(query);configLists.toArray()[0].getClass().getClassLoader();
-            if (!configLists.isEmpty()) {kernel.getClass().getClassLoader();
-                AbstractName on = (AbstractName) configLists.toArray()[0];AbstractName.class.getClassLoader();
+            Set<AbstractName> configLists = kernel.listGBeans(query);
+            if (!configLists.isEmpty()) {
+                AbstractName on = (AbstractName) configLists.toArray()[0];
                 try {
                     Boolean b = (Boolean) kernel.getAttribute(on, "kernelFullyStarted");
                     return b.booleanValue();
